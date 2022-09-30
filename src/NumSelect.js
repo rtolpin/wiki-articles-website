b3d3774df0bb508f4f99d9b61cb55401fe10ab99
Rebecca Tolpin <rebecca.tolpin@gmail.com>
2022-09-29 23:56:59 -0400

    number of results more

--- src/NumSelect.js
+++ src/NumSelect.js
@@ -19,6 +19,11 @@ function NumResultSelect(props){
                     <option value='300'>300</option>
                     <option value='400'>400</option>
                     <option value='500'>500</option>
+                    <option value='600'>600</option>
+                    <option value='700'>700</option>
+                    <option value='800'>800</option>
+                    <option value='900'>900</option>
+                    <option value='1000'>1000</option>
                 </Form.Select>
             </div>
         </>
